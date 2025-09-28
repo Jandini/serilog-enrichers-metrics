819b71d610870b463e17fdab3683c9c63c73e716
Matt Janda <matt.janda.kingston@gmail.com>
2025-09-28 22:47:20 +0530

    Code cleanup

--- src/Serilog.Enrichers.Metrics.IO/IoMetricsEnricherExtensions.cs
+++ src/Serilog.Enrichers.Metrics.IO/IoMetricsEnricherExtensions.cs
@@ -4,8 +4,6 @@ namespace Serilog;
 
 public static class IoMetricsEnricherExtensions
 {    
-    public static LoggerConfiguration WithIoMetrics(
-        this LoggerEnrichmentConfiguration enrich,
-        TimeSpan? minSampleInterval = null)
+    public static LoggerConfiguration WithIoMetrics(this LoggerEnrichmentConfiguration enrich, TimeSpan? minSampleInterval = null)
         => enrich.With(new IoMetricsEnricher(minSampleInterval));
 }
